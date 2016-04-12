cd805d4b0c8d904b08b5683579c2d6bbc3912a81
Jackson de Fraga <jackson.fraga86@gmail.com>
2016-04-12 00:05:38 -0300

    Ajuste no projeto

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,6 +1,14 @@
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+  router.get('/stats', server.loopback.status());
+  
+  router.get('/', function(req, res) {
+    res.redirect('/index.htm')
+  }); 
+  
+  router.get('/home', function(req, res) {
+     res.sendFile('index.html');
+  });  
   server.use(router);
 };
